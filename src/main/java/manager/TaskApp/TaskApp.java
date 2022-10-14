e882f0d23c172b3620860c1783e469b041e001b0
Rafa-Lino <rafaelino71@gmail.com>
2022-10-14 17:30:17 -0300

    Finalização dos testes com o BD, Tasks e Projects funcionando.

--- src/main/java/manager/TaskApp/TaskApp.java
+++ src/main/java/manager/TaskApp/TaskApp.java
@@ -1,8 +1,12 @@
 package manager.TaskApp;
 
 import controller.ProjectController;
+import controller.TaskController;
 import java.sql.SQLException;
+import java.util.Date;
+import java.util.List;
 import model.Project;
+import model.Task;
 
 /**
  *
@@ -14,9 +18,44 @@ public class TaskApp {
         
         ProjectController projectController = new ProjectController();
         
+       /* //criando novo projeto
         Project project = new Project();
-        project.setName("Teste");
-        project.setDescription("Descrição");
-        projectController.save(project); 
+        project.setId(1);
+        project.setName("TestandoTasks");
+        project.setDescription("Descrição aleatoria");
+        projectController.save(project);
+        
+        //projectController.update(project); */
+        
+        List<Project> projects = projectController.getAll();
+        System.out.println("Total de projetos: " + projects.size());
+        
+        // projectController.removeById(1);
+        
+        
+        //TESTE DE CRIAÇÃO DE TASKS
+        
+        TaskController taskController = new TaskController();
+        
+        //criando nvoas tasks
+        Task task = new Task();
+        task.setIdProject(3);
+        task.setId(1);
+        task.setName("VAI VARRER");
+        task.setDescription("Varre");
+        task.setNotes("tem que varrer");
+        task.setIsCompleted(false);
+        task.setDeadline(new Date());
+        
+        //taskController.save(task);
+               
+        taskController.update(task);
+        List<Task> tasks = taskController.getAll(3);
+        System.out.println("Total de tarefas: " + tasks.size());
+        
+       // taskController.removeById(1);
+        
+        
+        
     }
 }
